558519b4d50fdc67de2e92be6abbb55c7bb4d193
Ajumobi1 <barbararoark628@gmail.com>
2026-03-23 06:55:35 -0400

    fix: Select component now explicitly handles children for option elements

--- frontend/components/ui/select.tsx
+++ frontend/components/ui/select.tsx
@@ -2,9 +2,11 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-export interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {}
+export interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  children?: React.ReactNode;
+}
 
-const Select = React.forwardRef<HTMLSelectElement, SelectProps>(({ className, ...props }, ref) => {
+const Select = React.forwardRef<HTMLSelectElement, SelectProps>(({ className, children, ...props }, ref) => {
   return (
     <select
       className={cn(
@@ -14,7 +16,9 @@ const Select = React.forwardRef<HTMLSelectElement, SelectProps>(({ className, ..
       )}
       ref={ref}
       {...props}
-    />
+    >
+      {children}
+    </select>
   );
 });
 
